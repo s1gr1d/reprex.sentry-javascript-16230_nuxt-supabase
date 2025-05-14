89b64a539aeae2f5d1a48c70361694c3e9532296
s1gr1d <sigrid.huemer@posteo.at>
2025-05-14 11:29:17 +0200

    setup sentry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@sentry/nuxt/module'],
+
+  runtimeConfig: {
+    public: { sentryDsn: process.env.NUXT_PUBLIC_SENTRY_DSN }
+  },
 })
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+import * as Sentry from "@sentry/nuxt";
+import dotenv from 'dotenv';
+
+dotenv.config();
+ 
+Sentry.init({
+  dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: true,
+});
